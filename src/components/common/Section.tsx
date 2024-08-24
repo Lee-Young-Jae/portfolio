6f59c2e8c2552d7bb161187dbc1876a63fc7c195
Lee-Young-Jae <ori_@kakao.com>
2024-08-25 02:18:28 +0900

    refactor: 컴포넌트 분리

---
+++ src/components/common/Section.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+interface SectionProps {
+  id: string;
+  title?: string;
+  children?: ReactNode;
+}
+
+const Section = ({ id, title, children }: SectionProps) => {
+  return (
+    <Styled.SectionContainer id={id}>
+      <Styled.Section>
+        <Styled.Title>{title}</Styled.Title>
+        {children}
+      </Styled.Section>
+    </Styled.SectionContainer>
+  );
+};
+
+const Styled = {
+  SectionContainer: styled.div`
+    margin-top: 76px;
+  `,
+  Section: styled.section`
+    padding: 50px 0;
+    min-height: 100vh;
+    color: white;
+    margin-top: 76px;
+    padding: 20px;
+    min-height: 100vh;
+  `,
+  Title: styled.h2`
+    font-size: 36px;
+    margin-bottom: 30px;
+  `,
+};
+
+export default Section;
