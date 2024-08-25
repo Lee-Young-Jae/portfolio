13f6167bc981ea3a10dd1393cacb3b7139bb7479
Lee-Young-Jae <ori_@kakao.com>
2024-08-25 15:19:45 +0900

    feat: GRAY 컬러 상수 추가.

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,6 +2,9 @@ const colors = {
   BLACK: "#000000",
   WHITE: "#FFFFFF",
   MINT: "#64FFDA",
+  GRAY_300: "#E0E0E0",
+  GRAY_500: "#9E9E9E",
+  GRAY_700: "#616161",
 } as const;
 
 export { colors };
