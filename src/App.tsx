c92ccb0e785115e92ad7f0775d56f93ed5dca423
Lee-Young-Jae <ori_@kakao.com>
2024-09-15 16:10:41 +0900

    feat: 개발자 모드에 진입할 때, 이스터에그 메세지를 출력하는 기능을 추가한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import "./App.css";
 import AppContent from "./components/layout/AppContent";
 import Background from "./components/layout/Background";
+import useConsoleMessage from "./hooks/useConsoleMessage";
 
 function App() {
+  const messages = [
+    {
+      text: " 찾아주셔서 감사합니다, 개발자 Young Jae 입니다!🚀 ",
+      style: {
+        "font-size": "16px",
+        "font-weight": "bold",
+        "background-color": "#000000",
+        color: "#64FFDA",
+        "line-height": "3",
+        "text-align": "center",
+      },
+    },
+  ];
+
+  useConsoleMessage({ messages });
+
   return (
     <div className="App">
       <Background />
